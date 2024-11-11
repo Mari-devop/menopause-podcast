020c1a6b6c998f32e5e8ab7cc47deb3cdb842ed3
Mari-devop <mari.philon@gmail.com>
2024-11-11 20:07:36 +0200

    init: project

---
+++ index.js
@@ -0,0 +1,51 @@
+const burgerButton = document.querySelector('.header-section__burger');
+const menu = document.querySelector('.header-section__menu');
+
+burgerButton.addEventListener('click', () => {
+    menu.classList.toggle('active'); 
+    burgerButton.classList.toggle('active'); 
+});
+
+//Video - about us section 
+function openModal() {
+    const modal = document.getElementById("video-modal");
+    const videoFrame = document.getElementById("video-frame");
+
+    modal.style.display = "block";
+    videoFrame.src = "https://www.youtube.com/embed/m1hLf297wrI?si=UI00UzJGIEWbaokq";
+  
+    document.body.classList.add("no-scroll");
+    document.addEventListener("keydown", handleEscapeKey);
+  }
+  
+  function closeModal() {
+    const modal = document.getElementById("video-modal");
+    const videoFrame = document.getElementById("video-frame");
+  
+    modal.style.display = "none";
+    videoFrame.src = "";
+  
+    document.body.classList.remove("no-scroll");
+  
+    document.removeEventListener("keydown", handleEscapeKey);
+  }
+  
+  document.querySelector(".close").onclick = function () {
+    closeModal();
+  };
+  
+  window.onclick = function (event) {
+    const modal = document.getElementById("video-modal");
+    const modalContent = document.querySelector(".modal-content");
+  
+    if (event.target === modal && !modalContent.contains(event.target)) {
+      closeModal();
+    }
+  };
+  
+  function handleEscapeKey(event) {
+    if (event.key === "Escape") {
+      closeModal();
+    }
+  }
+  
